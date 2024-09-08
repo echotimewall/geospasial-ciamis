2da94b64fcad58047d5af3e513cc5bda3f7c57f7
nonokda <50684846+nonokda@users.noreply.github.com>
2024-09-08 14:55:32 +0700

    Add files via upload

--- admin/index.php
+++ admin/index.php
@@ -7,20 +7,27 @@ if(isset($_POST['submit'])){
     $user = strip_tags($_POST['username']);
     $pass = strip_tags($_POST['pass']);
 
-    $sql = 'select *
-            from t_users
-            where Username =? and Password = md5(?)';
-    //echo $sql;
-    $row = $config->prepare($sql);
-    $row -> execute(array($user,$pass));
-    $jum = $row -> rowCount();
-    if($jum > 0){
-        $hasil = $row -> fetch();
-        $_SESSION['admin'] = $hasil;
-        echo '<script>alert("Login Sukses");window.location="production/index.php"</script>';
-    }else{
-        echo '<script>alert("Login gagal. Username atau password tidak sesuai !");history.go(-1);</script>';
+    if ($_SESSION["code"] != $_POST["kdcaptcha"]) {
+        echo '<script>alert("Kode Captcha salah !");history.go(-1);</script>';        
+    } else {
+
+        $sql = 'select *
+                from t_users
+                where Username =? and Password = md5(?)';
+        //echo $sql;
+        $row = $config->prepare($sql);
+        $row -> execute(array($user,$pass));
+        $jum = $row -> rowCount();
+        if($jum > 0){
+            $hasil = $row -> fetch();
+            $_SESSION['admin'] = $hasil;
+            echo '<script>alert("Login Sukses");window.location="production/index.php"</script>';
+        }else{
+            echo '<script>alert("Login gagal. Username atau password tidak sesuai !");history.go(-1);</script>';
+        }
+            
     }
+
 }
 ?>
 <!DOCTYPE html>
@@ -57,8 +64,12 @@ if(isset($_POST['submit'])){
             <div class="col-lg-6">
                 <div class="card1 pb-5">
                     <div class="row">
-							<a href="index.php"><img src="../assets/img/ciamis.png" class="logo"><img src="../assets/img/bappeda.png" width="135px" height="45px" style="margin-top: 20px;"></a>
-							<!--<h6 style="margin-top: 20px; margin-left: 10px; font-weight: bold;">BADAN PERENCANAAN PEMBANGUNAN DAERAH<br />KABUPATEN CIAMIS</h6>-->
+                        <div class="col-md-10">
+							<a href="../index.php"><img src="../assets/img/ciamis.png" class="logo"><img src="../assets/img/bappeda.png" width="135px" height="45px" style="margin-top: 20px;"></a>
+                        </div>
+                        <div class="col-md-2">
+							<h6 style="margin-top: 33px; font-weight: bold;">Sign In</h6>
+                        </div>
                     </div>
                     <div class="row px-3 justify-content-center mt-4 mb-5 border-line">
                         <img src="https://i.imgur.com/uNGdWHi.png" class="image">
@@ -68,34 +79,39 @@ if(isset($_POST['submit'])){
             <div class="col-lg-6">
             	<form name="frmLogin" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
                 <div class="card2 card border-0 px-4 py-5">
-                    <div class="row mb-4 px-3">
+                    <!--<div class="row mb-4 px-3">
                         <h6 class="mb-0 mr-4 mt-2"><b>Sign in</b></h6>
-                    </div>
+                    </div>-->
                     <div class="row px-3">
                         <label class="mb-1"><h6 class="mb-0 text-sm">Username</h6></label>
-                        <input class="mb-4" type="text" name="username" placeholder="Enter a valid username">
+                        <input type="text" name="username" placeholder="Enter a valid username">
                     </div>
                     <div class="row px-3">
                         <label class="mb-1"><h6 class="mb-0 text-sm">Password</h6></label>
                         <input type="password" name="pass" placeholder="Enter password">
+                    </div>
+                    <div class="row px-3">
+                        <label class="mb-1"><h6 class="mb-0 text-sm">Captcha</h6></label>
+                        <input type="text" name="kdcaptcha" placeholder="Enter captcha">
+                        <img src="captcha.php" alt="gambar" />
                     </div>
                     <div class="row px-3 mb-4">
                         <div class="custom-control custom-checkbox custom-control-inline">
                             <input id="chk1" type="checkbox" name="chk" class="custom-control-input"> 
                             <label for="chk1" class="custom-control-label text-sm">Remember me</label>
                         </div>
-                        <a href="#" class="ml-auto mb-0 text-sm">Lupa password ?</a>
+                        <!--<a href="#" class="ml-auto mb-0 text-sm">Lupa password ?</a>-->
                     </div>
                     <div class="row mb-3 px-3">
                         <button type="submit" name="submit" class="btn btn-blue text-center">Login</button>
                     </div>
-                    <div class="row mb-4 px-3">
-                        <small class="font-weight-bold"><a href="index.php" class="text-danger ">Kembali kehalaman depan</a></small>
-                    </div>
+                    <!--<div class="row mb-4 px-3">
+                        <small class="font-weight-bold"><a href="../index.php" class="text-danger ">Kembali kehalaman depan</a></small>
+                    </div>-->
                 </div>
             </div>
         </div>
-        <div class="bg-blue py-4">
+        <div class="bg-blue py-2">
             <div class="row px-3">
                 <small class="ml-4 ml-sm-5 mb-2"><script>document.write(new Date().getFullYear());</script> &copy; Badan Perencanaan Pembangunan Daerah Kabupaten Ciamis</small>
             </div>
